0dbaa52e204cab0a46fe63cba9cdb48bc183f6fb
PriyanshuBh <priyanshubh2003@gmail.com>
2025-06-25 16:16:52 +0530

    feat: update database URL configuration to use environment variable

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   schema: './utils/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: "postgresql://Ai%20PrepBot_owner:npg_rnX2k4WwuTHy@ep-broad-pine-a4vi9ykm-pooler.us-east-1.aws.neon.tech/Ai%20PrepBot?sslmode=require"
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL as string
   },
 });
